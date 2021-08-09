525c066d5c84958c7d0a98e532362bd5811b314a
Clark Wagdy <clarkwagdy1@gmail.com>
2021-08-10 01:11:55 +0200

    home done

---
+++ JS/main.js
@@ -0,0 +1,58 @@
+var textWrapper = document.querySelector('.home-title .letter');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.home-title .letter',
+    scale: [0, 1],
+    duration: 1500,
+    elasticity: 600,
+    delay: (el, i) => 45 * (i+1)
+  }).add({
+    targets: '.home-title',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
+
+var textWrapper = document.querySelector('.home-p .letter');
+textWrapper.innerHTML = textWrapper.textContent.replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.home-p .line',
+    scaleY: [0,1],
+    opacity: [0.5,1],
+    easing: "easeOutExpo",
+    duration: 700
+  })
+  .add({
+    targets: '.home-p .line',
+    translateX: [0, document.querySelector('.home-p .letter').getBoundingClientRect().width + 10],
+    easing: "easeOutExpo",
+    duration: 700,
+    delay: 100
+  }).add({
+    targets: '.home-p .letter',
+    opacity: [0,1],
+    easing: "easeOutExpo",
+    duration: 600,
+    offset: '-=775',
+    delay: (el, i) => 34 * (i+1)
+  }).add({
+    targets: '.home-p',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
+
+
+// screen change
+$(".screen1").click(function(){
+    window.location.href="HTML/page1.html";
+})
+$(".screen2").click(function(){
+    window.location.href="HTML/page2.html";
+})
